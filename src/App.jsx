6f9fc3b5b6a1d1e11717aebfe5a8dd4ac977450b
Edwin <edwincastaneda0801@gmail.com>
2024-10-02 13:05:11 -0500

    new color yellow added to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
+import Navbar from "./Components/Navbar.jsx";
 import Red from "./Components/Red.jsx";
 import Blue from "./Components/Blue.jsx";
 import Home from "./Components/Home.jsx";
+import Yellow from "./Components/Yellow.jsx";
 
 function App() {
   return (
@@ -12,12 +14,14 @@ function App() {
         <Link to={"/"}>HOME</Link>
         <Link to={"/blue"}>BLUE</Link>
         <Link to={"/red"}>RED</Link>
+        <Link to={"/yellow"}>YELLOW</Link>
       </div>
       <div id="main-section">
         <Routes>
           <Route path="/" element={Home()} />
           <Route path="/blue" element={Blue()} />
           <Route path="/red" element={Red()} />
+          <Route path="/yellow" element={Yellow()} />
         </Routes>
       </div>
     </div>
