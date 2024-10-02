359bbf35e359b7c8d36e3a1857320e61cc8b5c8a
Edwin <edwincastaneda0801@gmail.com>
2024-10-02 12:59:37 -0500

    home component to have a home page and added css to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react";
 import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
-import Red from "./components/Red.jsx";
-import Blue from "./components/Blue.jsx";
+import Red from "./Components/Red.jsx";
+import Blue from "./Components/Blue.jsx";
+import Home from "./Components/Home.jsx";
 
 function App() {
   return (
     <div id="container">
       <div id="navbar">
+        <Link to={"/"}>HOME</Link>
         <Link to={"/blue"}>BLUE</Link>
         <Link to={"/red"}>RED</Link>
       </div>
       <div id="main-section">
         <Routes>
+          <Route path="/" element={Home()} />
           <Route path="/blue" element={Blue()} />
           <Route path="/red" element={Red()} />
         </Routes>
